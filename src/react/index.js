d253654010bbd052dc5d9524d4a3a24e8e9e6206
Christian Lewis <ctjl96@gmail.com>
2020-05-28 21:37:50 -0500

    BULK AUTOMATIC COMMIT

--- src/react/index.js
+++ src/react/index.js
@@ -1,1 +1,77 @@
-export { default } from './typewriter';
+import React from 'react';
+import TypewriterCore from '../core';
+import isEqual from 'lodash/isEqual';
+
+class Typewriter extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.parseProps(this.props);
+    this.state = {
+      instance: null,
+    };
+  }
+
+  parseProps() {
+    const { strings, ...config } = this.props;
+    this.strings = strings;
+    this.config = config;
+    return this;
+  }
+
+  async stop() {
+    console.log("[DEBUG:stop] Trying to stop instance...", this.state);
+
+    this.setState(async () => {
+      if (this.state.instance)
+        await this.state.instance.stop();
+    });
+
+    return this;
+  }
+
+  async refreshInstance() {
+
+    console.log("[DEBUG:refreshInstance] Refreshing Typewriter...");
+    await this.stop();
+    
+    this.parseProps(this.props).setState({
+      instance: new TypewriterCore(
+          this.typewriter,
+          typeof this.strings == "string"
+            ? JSON.parse(this.strings)
+            : this.strings, 
+          this.config
+        )
+    });
+  }
+
+  async componentDidMount() {
+    console.log("[DEBUG:componentDidMount]");
+    await this.refreshInstance();
+  }
+
+  async componentDidUpdate(prevProps) {
+    console.log("[DEBUG:componentDidUpdate]");
+    if(!isEqual(this.props, prevProps))
+      await this.refreshInstance();
+  }
+
+  async componentWillUnmount() {
+    console.log("DEBUG:componentWillUnmount");
+    await this.stop();
+  }
+
+  render() {
+    const config = this.props.config || {};
+    return React.createElement(
+      'div',
+      {
+        ref: (ref) => this.typewriter = ref,
+        className: 'typewriter'
+      }
+    );
+  }
+}
+
+export default Typewriter;
