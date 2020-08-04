35175286b40c3957b0fe535e5a3c288c17bb3294
Christian Lewis <ctjl96@gmail.com>
2020-08-04 09:11:18 -0500

    init

--- webpack/development.config.babel.js
+++
@@ -1,9 +0,0 @@
-import merge from 'webpack-merge';
-import commonConfig from './common.config.babel';
-
-const config = merge(commonConfig, {
-  mode: 'development',
-  watch: true,
-});
-
-export default config;
